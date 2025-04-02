de1f1d5637fd4c5fa503ddd5200b0c9cfcb0d2b2
Sajid-Zubair <sajid10zubair@gmail.com>
2025-04-02 19:13:27 +0530

    #

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,9 +8,9 @@ function Navbar() {
                 <h3>TechFluent</h3>
             </div>
             <ul className='flex flex-grow justify-center space-x-8 text-gray-700'>
-                <li className='hover:text-blue-500 cursor-pointer hover:translate-y-[-2px] transition duration-200 ease-in-out' >Home</li>
-                <li className='hover:text-blue-500 cursor-pointer hover:translate-y-[-2px] transition duration-200 ease-in-out' >About</li>
-                <li className='hover:text-blue-500 cursor-pointer hover:translate-y-[-2px] transition duration-200 ease-in-out' >Resources</li>
+                <li className='hover:text-blue-500 cursor-pointer hover:translate-y-[-2px] transition duration-200 ease-in-out'>Home</li>
+                <li className='hover:text-blue-500 cursor-pointer hover:translate-y-[-2px] transition duration-200 ease-in-out'>About</li>
+                <li className='hover:text-blue-500 cursor-pointer hover:translate-y-[-2px] transition duration-200 ease-in-out'>Resources</li>
             </ul>
                 <button className='bg-blue-500 text-white rounded-2xl px-4 py-2 hover:bg-blue-600 transition hover:shadow-lg shadow-blue-900/50 hover:translate-y-[-2px] transition duration-200 ease-in-out'>Login</button>
         </div>
