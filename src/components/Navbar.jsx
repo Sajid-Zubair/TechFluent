18ace40b74a3cb8f169b69097183e8e78d9c0a5e
Sajid-Zubair <sajid10zubair@gmail.com>
2025-04-02 18:40:41 +0530

    added styles to the navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,11 +8,11 @@ function Navbar() {
                 <h3>TechFluent</h3>
             </div>
             <ul className='flex flex-grow justify-center space-x-8 text-gray-700'>
-                <li className='hover:text-blue-500 cursor-pointer'>Home</li>
-                <li className='hover:text-blue-500 cursor-pointer'>About</li>
-                <li className='hover:text-blue-500 cursor-pointer'>Resources</li>
+                <li className='hover:text-blue-500 cursor-pointer hover:translate-y-[-2px] transition duration-200 ease-in-out'>Home</li>
+                <li className='hover:text-blue-500 cursor-pointer hover:translate-y-[-2px] transition duration-200 ease-in-out'>About</li>
+                <li className='hover:text-blue-500 cursor-pointer hover:translate-y-[-2px] transition duration-200 ease-in-out'>Resources</li>
             </ul>
-                <button className='bg-blue-500 text-white rounded-2xl px-4 py-2 hover:bg-blue-600 transition'>Login</button>
+                <button className='bg-blue-500 text-white rounded-2xl px-4 py-2 hover:bg-blue-600 transition hover:shadow-lg shadow-blue-900/50 hover:translate-y-[-2px] transition duration-200 ease-in-out'>Login</button>
         </div>
     </>
   )
