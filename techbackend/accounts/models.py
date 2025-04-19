987e0922c6921b40d26e714388a9898c8409ec86
Sajid-Zubair <sajid10zubair@gmail.com>
2025-04-19 19:55:07 +0530

    created the user model

--- techbackend/accounts/models.py
+++ techbackend/accounts/models.py
@@ -1,3 +1,23 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractUser
 # Create your models here.
+
+class CustomUser(AbstractUser):
+    # Extra fields
+    college_name = models.CharField(max_length=100)
+    year_of_joining = models.CharField(max_length=100)
+
+    # Interview tracking
+    technical_interviews = models.PositiveIntegerField(default=0)
+    behavioral_interviews = models.PositiveIntegerField(default=0)
+    total_interviews = models.PositiveIntegerField(default=0)
+    overall_rating = models.FloatField(default=0.0)
+
+    # Individual ratings
+    fluency = models.FloatField(default=0.0)
+    coherence = models.FloatField(default=0.0)
+    accuracy = models.FloatField(default=0.0)
+    grammar = models.FloatField(default=0.0)
+
+    def __str__(self):
+        return self.username
