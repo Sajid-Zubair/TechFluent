87c461dd6f41b3e90c620926cd1f129f2119a190
Sajid-Zubair <sajid10zubair@gmail.com>
2025-04-22 23:10:48 +0530

    made changes to the resources section

--- src/components/Resources.jsx
+++ src/components/Resources.jsx
@@ -7,21 +7,24 @@ function Resources() {
     <div>
       <Sidenav />
       <div className='ml-64 flex flex-col items-center w-full'>
-        <div className='p-6 flex flex-row justify-space-between items-center gap-6 w-full max-w-4xl'>
-          <h2 className='text-3xl font-bold'>Resource Suggestions</h2>
-          <div className='flex items-center gap-2'>
+        <div className='p-6 flex flex-row justify-between items-center gap-6 w-full max-w-4xl'>
+          <div className='flex flex-col justify-between items-center gap-3'>
+            <h2 className='text-3xl font-bold'>Get Your Questions Answered in Seconds!</h2>
+            <p className='text-gray-600 text-md'>Unlock Your Potential with Knowledge</p>
+          </div>
+        </div>
+        <div className='flex flex-col items-center justify-center w-full'>
+          <div className='flex flex-row items-center justify-center gap-2 w-2xl max-w-xl mr-64'>
             <input
-              className='p-2 border-2 border-gray-500 rounded-lg'
+              className='p-3 border-2 border-gray-500 rounded-lg focus:outline-none focus:border-blue-500 focus:ring-1 focus:ring-blue-500 transition duration-200 w-full'
               type="text"
               placeholder='What do you want to learn?'
             />
-            <button>
-              <FaSearch size={24} className='text-gray-600 cursor-pointer' />
+            <button className='p-3 bg-blue-500 text-white rounded-lg shadow-lg hover:bg-blue-600 transition-all cursor-pointer'>
+              Search
             </button>
           </div>
         </div>
-
-        
       </div>
     </div>
   )
