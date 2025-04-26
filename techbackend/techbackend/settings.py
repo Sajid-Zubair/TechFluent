6b4bd7105f693d792d9f1cb60865a021adbedfc5
Sajid-Zubair <sajid10zubair@gmail.com>
2025-04-26 23:31:19 +0530

    made the signup tables

--- techbackend/techbackend/settings.py
+++ techbackend/techbackend/settings.py
@@ -80,8 +80,12 @@ WSGI_APPLICATION = "techbackend.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        'NAME': 'techdb',
+        'USER': 'postgres',
+        'PASSWORD': 'kidshot123',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
