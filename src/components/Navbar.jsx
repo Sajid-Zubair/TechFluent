66a87aaf55eb3a3e8ab1d4a53f4112746f666b7a
Sajid-Zubair <sajid10zubair@gmail.com>
2025-05-06 01:20:36 +0530

    made the links bold

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,12 +7,12 @@ function Navbar() {
     <>
         <div className='flex justify-between items-center bg-blue-100 rounded-lg p-2 m-4 shadow-md'>
             <div>
-                <h3 className='text-3xl font-bold text-blue-500 font-serif cursor-pointer'>TechFluent</h3>
+                <h3 className='text-3xl font-bold text-blue-500 cursor-pointer'>TechFluent</h3>
             </div>
             <ul className='flex flex-grow justify-center space-x-8 text-gray-700'>
-                <Link className='hover:text-blue-500 cursor-pointer hover:translate-y-[-2px] transition duration-200 ease-in-out'>Home</Link>
-                <Link className='hover:text-blue-500 cursor-pointer hover:translate-y-[-2px] transition duration-200 ease-in-out'>About</Link>
-                <Link className='hover:text-blue-500 cursor-pointer hover:translate-y-[-2px] transition duration-200 ease-in-out'>Resources</Link>
+                <Link className='hover:text-blue-500 cursor-pointer hover:translate-y-[-2px] transition duration-200 ease-in-out font-semibold'>Home</Link>
+                <Link className='hover:text-blue-500 cursor-pointer hover:translate-y-[-2px] transition duration-200 ease-in-out font-semibold'>About</Link>
+                <Link className='hover:text-blue-500 cursor-pointer hover:translate-y-[-2px] transition duration-200 ease-in-out font-semibold'>Resources</Link>
             </ul>
             <button onClick={() => navigate('/login')} className=" px-6 py-3 bg-blue-500 text-white rounded-lg shadow-lg hover:bg-blue-600 transition-all cursor-pointer">
               Login
