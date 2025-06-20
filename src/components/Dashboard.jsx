5e0a97ec314c50827178c0ff21d38f0696dec99e
Sajid-Zubair <sajid10zubair@gmail.com>
2025-06-20 14:30:07 -0400

    #

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -138,6 +138,7 @@ function Dashboard() {
         <div className="fixed top-0 left-0 w-64 h-full bg-white shadow-lg z-40 p-4">
           <Sidenav />
           <button
+            name="close"
             onClick={() => setMobileNavOpen(false)}
             className="absolute top-2 right-2 text-2xl text-red-600 mt-4 ml-6"
           >
