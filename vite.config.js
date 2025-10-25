8c729b22829bbbc6ff27b0eab4d9e0cc57e6781c
Sajid-Zubair <sajid10zubair@gmail.com>
2025-10-25 10:37:10 +0530

    updated vite.config file

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,28 @@
+// import tailwindcss from '@tailwindcss/vite'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [
+//     react(),tailwindcss(),
+//   ],
+
+//   server: {
+//     proxy: {
+//       '/api': {
+//         target: 'http://localhost:8000',
+//         changeOrigin: true,
+//       }
+//     }
+//   }
+
+//   // build: {
+//   //   outDir: 'dist',
+//   //   assetsDir: 'assets',
+//   // },
+// })
+
 import tailwindcss from '@tailwindcss/vite'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
@@ -5,7 +30,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
-    react(),tailwindcss(),
+    react(),
+    tailwindcss(),
   ],
 
   server: {
@@ -15,10 +41,11 @@ export default defineConfig({
         changeOrigin: true,
       }
     }
-  }
+  },
 
-  // build: {
-  //   outDir: 'dist',
-  //   assetsDir: 'assets',
-  // },
+  preview: {
+    allowedHosts: ['techfluent.onrender.com'], // 👈 allows your Render domain
+    host: true, // binds to 0.0.0.0 (needed for Render)
+    port: 10000 // same port you used in Render start command
+  }
 })
